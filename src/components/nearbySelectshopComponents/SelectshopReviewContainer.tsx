c132316ee6e5930c2acdc0bdb6037b51ffff6ed9
hwang daeseong <klp2797@naver.com>
2024-09-29 20:48:29 +0900

    style:리뷰 작성하기 스타일 수정

--- src/components/nearbySelectshopComponents/SelectshopReviewContainer.tsx
+++ src/components/nearbySelectshopComponents/SelectshopReviewContainer.tsx
@@ -23,7 +23,7 @@ const SelectshopReviewContainer = ({ id, onWriteReviewClick }: PropsType) => {
               alert("로그인이 필요한 서비스 입니다.");
               return;
             }
-            onWriteReviewClick(); // 콜백 호출
+            onWriteReviewClick();
           }}
           variant="contained"
           sx={{ padding: "5px 30px" }}
@@ -40,7 +40,7 @@ export default SelectshopReviewContainer;
 const S = {
   SelectshopReviewContainer: styled.div`
     border-top: solid 1px #999;
-    padding: 20px 18px 30px 18px;
+    padding: 20px 12px 30px 12px;
   `,
   SelectshopReviewTitle: styled.h1`
     ${styleFont.textLarge}
--- src/pages/writeReview/index.tsx
+++ src/pages/writeReview/index.tsx
@@ -2,6 +2,7 @@ import WriteReviewInputImage from "@/components/writeReviewComponents/WriteRevie
 import useLoginUserId from "@/hook/useLoginUserId";
 import supabase from "@/lib/supabaseClient";
 import { styleColor } from "@/styles/styleColor";
+import { styleFont } from "@/styles/styleFont";
 import { registerReviewSchema } from "@/validators/review";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@mui/material";
@@ -10,6 +11,7 @@ import React, { useState } from "react";
 import { useForm, useFieldArray, SubmitHandler } from "react-hook-form";
 import shortid from "shortid";
 import styled from "styled-components";
+import Trash from "@/assets/Trash.svg";
 
 export interface ReviewType {
   files: FileList | null | undefined;
@@ -61,7 +63,6 @@ const WriteReview = () => {
     disAdvantage,
     tags,
   }: ReviewType) => {
-
     let imagesString: string[] = [];
     if (files) {
       for (const file of files) {
@@ -72,13 +73,15 @@ const WriteReview = () => {
           await supabase.storage
             .from("images")
             .upload(`reviewImages/${randomFileName}`, file);
-          imagesString.push(`${process.env.NEXT_PUBLIC_SUPABASE_STORAGE}/reviewImages/${randomFileName}`);
+          imagesString.push(
+            `${process.env.NEXT_PUBLIC_SUPABASE_STORAGE}/reviewImages/${randomFileName}`
+          );
         } catch (error) {
           console.error("Unexpected error:", error);
         }
       }
     }
-    
+
     const newReview = {
       selectshopId: id,
       reviewImages: files === null ? null : imagesString.join(","),
@@ -98,23 +101,18 @@ const WriteReview = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(addReviewSubmit)}>
-        <div>
-          <h1>후기 등록하기</h1>
-        </div>
-        <ul>
-          <li>
-            <WriteReviewInputImage
-              files={files}
-              setFiles={setFiles}
-            />
-          </li>
-          <li>
+    <S.WriteReviewContainer>
+      <S.WriteReviewInner onSubmit={handleSubmit(addReviewSubmit)}>
+        <S.WriteReviewTitle>✍ 후기 등록하기</S.WriteReviewTitle>
+        <S.WriteReviewUl>
+          <S.InputLiRow>
+            <WriteReviewInputImage files={files} setFiles={setFiles} />
+          </S.InputLiRow>
+          <S.InputLiRow>
             <S.Label htmlFor="review">후기</S.Label>
-            <S.Input id="review" {...register("review")}></S.Input>
-          </li>
-          <li>
+            <S.TextArea id="review" {...register("review")} />
+          </S.InputLiRow>
+          <S.InputLiRow>
             <S.Label htmlFor="advantage">
               장점
               <S.AddButton
@@ -131,12 +129,14 @@ const WriteReview = () => {
                   {...register(`advantage.${index}.value`)}
                 />
                 {index > 0 && (
-                  <button onClick={() => advantageRemove(index)}>삭제</button>
+                  <button onClick={() => advantageRemove(index)}>
+                    <Trash fill={`${styleColor.GRAY[200]}`} />
+                  </button>
                 )}
               </div>
             ))}
-          </li>
-          <li>
+          </S.InputLiRow>
+          <S.InputLiRow>
             <S.Label htmlFor="disAdvantage">
               단점
               <S.AddButton
@@ -155,44 +155,87 @@ const WriteReview = () => {
                   ></S.Input>
                   {index > 0 && (
                     <button onClick={() => disAdvantageRemove(index)}>
-                      삭제
+                      <Trash fill={`${styleColor.GRAY[200]}`} />
                     </button>
                   )}
                 </div>
               );
             })}
-          </li>
-          <li>
+          </S.InputLiRow>
+          <S.InputLiRow>
             <S.Label htmlFor="brand">추천 브랜드</S.Label>
             <S.Input
               id="brand"
               placeholder="쉼표(,)로 구분하면 멋진 태그가 될거에요!"
               {...register("tags")}
             ></S.Input>
-          </li>
-        </ul>
+          </S.InputLiRow>
+        </S.WriteReviewUl>
         <Button type="submit">저장</Button>
-      </form>
-    </div>
+      </S.WriteReviewInner>
+    </S.WriteReviewContainer>
   );
 };
 
 export default WriteReview;
 
 const S = {
-  Label: styled.label`
-    display: block;
+  WriteReviewContainer: styled.div``,
+  WriteReviewInner: styled.form`
+    padding: 20px 0px;
+  `,
+  WriteReviewTitle: styled.h1`
+    ${styleFont.textLarge}
     margin-bottom: 15px;
+  `,
+  WriteReviewUl: styled.ul`
+    li:first-child {
+      padding: 0px;
+    }
+  `,
+  InputLiRow: styled.li`
+    padding: 0px 12px;
+    margin-bottom: 15px;
+    div {
+      position: relative;
+      left: 0;
+      top: 0;
+      button {
+        position: absolute;
+        right: 6px;
+        top: 12px;
+        cursor: pointer;
+      }
+    }
+  `,
+  Label: styled.label`
+    display: flex;
+    align-items: center;
+    gap: 7px;
+    margin-bottom: 10px;
+    ${styleFont.textLarge}
   `,
   ImageInput: styled.input`
     display: none;
   `,
-  Input: styled.input`
+  TextArea: styled.textarea`
     width: 100%;
-    padding: 18px 0px;
+    padding: 12px 6px;
+    border: solid 1px ${styleColor.GRAY[200]};
+    border-radius: 4px;
+    outline: none;
+    &::placeholder {
+      font-size: 14px;
+      color: #d9dfeb;
+    }
+  `,
+  Input: styled.input`
+    display: block;
+    width: 100%;
+    padding: 12px 0px;
     margin-bottom: 7px;
-    text-indent: 6px;
-    border: solid 1px #d9dfeb;
+    /* text-indent: 6px; */
+    border: solid 1px ${styleColor.GRAY[200]};
     border-radius: 4px;
     outline: none;
     &::placeholder {
@@ -206,6 +249,5 @@ const S = {
     background-color: ${styleColor.INDIGO[0]};
     color: #fff;
     border-radius: 4px;
-    margin-left: 7px;
   `,
 };
