690c75c7c0816136305034a3e546cf5c01db70b0
unknown <klp2797@naver.com>
2025-01-03 02:03:02 +0900

    chore: build env update

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,10 @@ const nextConfig = {
     compiler:{
       styledComponents:true,
     },
+    env: {
+      NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+      NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY,
+    },
   };
 
 export default nextConfig;
